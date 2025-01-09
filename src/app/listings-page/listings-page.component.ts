7ddb1e82de2d4a338489bd3fd403ec23f51ff460
Ricardo Patino <rpatino12@outlook.com>
2025-01-09 18:58:35 -0500

    Create and add the first Angular component

---
+++ src/app/listings-page/listings-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listings-page',
+  standalone: false,
+  
+  templateUrl: './listings-page.component.html',
+  styleUrl: './listings-page.component.css'
+})
+export class ListingsPageComponent {
+
+}

5739521c5455a26402d4f49bb544b72916758454
Ricardo Patino <rpatino12@outlook.com>
2025-01-09 19:17:34 -0500

    Add the other pages for the app and create the routing for each page

---
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-detail-page',
+  standalone: false,
+  
+  templateUrl: './listing-detail-page.component.html',
+  styleUrl: './listing-detail-page.component.css'
+})
+export class ListingDetailPageComponent {
+
+}

3e3ca59d808cadf3e0d6ec3fd2040e50963e9e7b
Ricardo Patino <rpatino12@outlook.com>
2025-01-10 12:50:20 -0500

    Add the title for detail pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterModule, Routes } from '@angular/router';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
 import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
 import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
+import { fakeListings } from './fake-data';
 
 const routes: Routes = [
   {
@@ -16,11 +17,13 @@ const routes: Routes = [
   {
     path: 'listings',
     component: ListingsPageComponent,
+    title: 'BuyAndSell | All listings',
     pathMatch: 'full'
   },
   {
     path: 'listings/:id',
-    component: ListingDetailPageComponent
+    component: ListingDetailPageComponent,
+    title: (route: ActivatedRouteSnapshot) => `BuyAndSell | ${fakeListings.find(listing => listing.id === route.paramMap.get('id'))?.name}`
   },
   {
     path: 'contact/:id',

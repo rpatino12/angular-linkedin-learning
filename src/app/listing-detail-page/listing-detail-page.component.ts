afef54bc1bf9fb69499f75d9d0a42d47c78dab21
Ricardo Patino <rpatino12@outlook.com>
2025-01-10 12:31:06 -0500

    Create a listing detail page

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -7,6 +10,17 @@ import { Component } from '@angular/core';
   templateUrl: './listing-detail-page.component.html',
   styleUrl: './listing-detail-page.component.css'
 })
-export class ListingDetailPageComponent {
+export class ListingDetailPageComponent implements OnInit {
+  
+  listing: Listing | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
+  }
 
 }

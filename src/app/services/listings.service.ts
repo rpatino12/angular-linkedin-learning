25fd77bc95217ded1d6ecea12c72dc0fff1e623a
Ricardo Patino <rpatino12@outlook.com>
2025-01-14 12:53:08 -0500

    Implement the HttpClient and RXJS modules to send the request to the API subscribing to an observable

--- src/app/services/listings.service.ts
+++ src/app/services/listings.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { fakeListings } from '../fake-data';
 import { Listing } from '../types';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListingsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  getListings(): Listing[] {
-    return fakeListings;
+  getListings(): Observable<Listing[]> {
+    return this.http.get<Listing[]>('api/listings');
   }
 }

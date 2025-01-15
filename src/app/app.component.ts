abf3c955e4a6438d8e51371508d553dac3e277a6
Ricardo Patino <rpatino12@outlook.com>
2025-01-15 18:10:31 -0500

    Add Sign In and Sign Out with Google account using Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { getAuth, signInWithPopup, signOut, GoogleAuthProvider, UserCredential, User } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'buy-and-sell-site';
+  auth = getAuth();
+  user: User | undefined;
+  
+  signInClicked(): void {
+    signInWithPopup(this.auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (credential) {
+          const token = credential.accessToken;
+          // The signed-in user info.
+          this.user = result.user;
+          // IdP data available using getAdditionalUserInfo(result)
+          // ...
+        }
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
+
+  signOutClicked(): void {
+    signOut(this.auth).then(() => {
+      this.user = undefined;
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
 }
